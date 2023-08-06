4a72ccf1d7564a00dcb1e006ec2389c84cd9c957
Oleg <init.go@gmail.com>
2023-08-06 23:10:44 +0300

    Task0

---
+++ task00/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введённое число: ");
+Console.WriteLine(number);
+Console.Write("Квадрат числа: ");
+Console.WriteLine(number*number);
