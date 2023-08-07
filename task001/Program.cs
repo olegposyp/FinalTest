d598c47a19b0dc4f482ff9e3e1df1c8d21e2b26d
Oleg <init.go@gmail.com>
2023-08-07 09:19:38 +0300

    Task 001

---
+++ task001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2*number2)
+    {
+Console.Write("Число "); Console.Write(number1); Console.Write(" является квадратом числа "); Console.WriteLine(number2);
+    }
+else
+Console.Write("Число "); Console.Write(number1); Console.Write(" не является квадратом числа "); Console.WriteLine(number2);
