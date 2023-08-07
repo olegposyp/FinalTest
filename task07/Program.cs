9080b0240594bd57a5ee604cf7b89f74b1d1ceb4
Oleg <init.go@gmail.com>
2023-08-07 10:06:15 +0300

    Task07

---
+++ task07/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+Console.WriteLine("Не трёхзначное число");
