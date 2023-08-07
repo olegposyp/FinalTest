dcbbe74578b335ab6c9b21a6d5cb6d85631c59a9
Oleg <init.go@gmail.com>
2023-08-07 11:05:21 +0300

    Task02

---
+++ task02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if (numbera > numberb)
+{
+    Console.WriteLine("Число: " + numbera + " больше числа " + numberb);
+}
+else
+if (numbera < numberb)
+{
+    Console.WriteLine("Число: " + numberb + " больше числа " + numbera);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
