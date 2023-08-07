0f559036f2f903521bf1e63cdc50fca18499909b
Oleg <init.go@gmail.com>
2023-08-07 23:00:46 +0300

    Task06

---
+++ task06/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine("Число "+num+" чётное");
+}
+else Console.WriteLine("Число "+num+" нечётное");
