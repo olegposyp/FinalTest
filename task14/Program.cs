f8bfd400316a82857b323f0b9f3dd640ea1e5c13
Oleg <init.go@gmail.com>
2023-08-08 13:15:25 +0300

    Task14

---
+++ task14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+
+if (num % num1 == 0 & num % num2 == 0)
+{
+    Console.WriteLine($"Число {num} кратно числам {num1} и {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно числам {num1} и {num2}");
+}
