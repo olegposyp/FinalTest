8266b72765a6e526c1732340f31ff613b2295f2a
Oleg <init.go@gmail.com>
2023-08-08 12:32:19 +0300

    Task09

---
+++ task09/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10,100);
+Console.WriteLine(num);
+int n1 = num/10;
+int n2 = num%10;
+if (n1>n2)
+{
+    Console.WriteLine($"Наибольшая цифра числа {num} - {n1}");
+}
+else Console.WriteLine($"Наибольшая цифра числа {num} - {n2}");
+if (n1==n2)
+{
+    Console.WriteLine($"Обе цифры одинаковы {n1}");
+}
