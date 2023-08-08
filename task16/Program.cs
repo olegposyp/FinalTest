12d1948ff7490958448c581aa44b852d9efa1237
Oleg <init.go@gmail.com>
2023-08-08 13:25:40 +0300

    Task16

---
+++ task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2*num2)
+{
+Console.WriteLine($"Число {num1} - это квадрат числа {num2}");
+}
+else if (num2 == num1*num1)
+{
+Console.WriteLine($"Число {num2} - это квадрат числа {num1}");
+}
+else Console.WriteLine($"Числа {num2} и {num1} не являются квадратами друг друга");
