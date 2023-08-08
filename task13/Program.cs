1728444f398eb88fc592579bd46f3bed8ba66699
Oleg <init.go@gmail.com>
2023-08-08 13:04:29 +0300

    Task13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1%num2 == 0)
+{
+Console.WriteLine($"Число {num1} кратно числу {num2}");
+}
+else 
+{
+    Console.Write($"Число {num1} не кратно числу {num2} ");
+    int num3 = num1%num2;
+    Console.WriteLine($"Остаток деления - {num3}");
+}
