98b946f203eafe7f3a463c5ded7cc10e22738d63
Oleg <init.go@gmail.com>
2023-08-08 13:49:56 +0300

    Task10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 0;
+if (num < 100 || num > 999)
+{
+    Console.WriteLine($"Число {num} - не трёхзначное");
+}
+else
+{
+    num1 = num / 10;
+num1 = num1 % 10;
+Console.WriteLine($"Вторая цифра числа {num} - это {num1}");
+}
