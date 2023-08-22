f284f5891e1043c9b722b34e793faf8170db1b1b
Oleg <init.go@gmail.com>
2023-08-22 11:55:23 +0300

    task 18

---
+++ task18/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    System.Console.WriteLine($"Точки в четверти {num} имеют координаты: X > 0, Y > 0 ");
+}
+else if (num == 2)
+{
+    System.Console.WriteLine($"Точки в четверти {num} имеют координаты: X < 0, Y > 0 ");
+}
+else if (num == 3)
+{
+    System.Console.WriteLine($"Точки в четверти {num} имеют координаты: X < 0, Y < 0 ");
+}
+else if (num == 4)
+{
+    System.Console.WriteLine($"Точки в четверти {num} имеют координаты: X > 0, Y < 0 ");
+}
+else 
+{
+    System.Console.WriteLine($"Такой четверти не существует");
+}
