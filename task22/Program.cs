e5c73f37e8c94eb99a70cff105707ced462a150f
Oleg <init.go@gmail.com>
2023-08-22 11:33:15 +0300

    task 22

---
+++ task22/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int sq = 0;
+while (count < num)
+{
+    count++;
+    sq = count*count;
+    System.Console.Write(sq);
+}
+
