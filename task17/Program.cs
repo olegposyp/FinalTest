e1a5f9775032a0777652a3405fb68b61225c006e
Oleg <init.go@gmail.com>
2023-08-22 10:41:51 +0300

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите X и Y Вашей точки");
+int userx = Convert.ToInt32(Console.ReadLine());
+int usery = Convert.ToInt32(Console.ReadLine());
+if (userx > 0 && usery > 0)
+{
+    System.Console.WriteLine($"Точка {userx} : {usery} находится в первой четверти");
+}
+else if (userx < 0 && usery > 0)
+{
+    System.Console.WriteLine($"Точка {userx} : {usery} находится во второй четверти");
+}
+else if (userx < 0 && usery < 0)
+{
+    System.Console.WriteLine($"Точка {userx} : {usery} находится в третьей четверти");
+}
+else if (userx > 0 && usery < 0)
+{
+    System.Console.WriteLine($"Точка {userx} : {usery} находится в четвёртой четверти");
+}
