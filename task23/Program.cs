f4fd4d4dc9368f1bde4d844dae9cd734baa3c086
Oleg <init.go@gmail.com>
2023-08-28 16:29:02 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int cube = 0;
+while (count < num)
+{
+    count++;
+    cube = count*count*count;
+    System.Console.Write($"{cube} ,");
+}
+
