6b7905198cc84bfc200cd547953c32654fb1c84a
Oleg <init.go@gmail.com>
2023-08-28 10:28:34 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+int palin = Convert.ToInt32(Console.ReadLine());
+if (palin <10000 || palin > 99999)
+{
+    System.Console.WriteLine("Число не пятизначное");
+}
+else
+{int num = palin;
+int vv = 0;
+while (num > 10)
+{
+    vv = vv + num%10;
+    vv = vv*10;
+    num = num / 10;
+}
+    vv = vv + num;
+if (vv == palin)
+{
+    System.Console.WriteLine($"Число {palin} - это палиндром");
+}
+else
+    System.Console.WriteLine($"Число {palin} - это не палиндром");
+}
