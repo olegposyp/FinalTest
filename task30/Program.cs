7323f7a919f89340c9f90c09d7920f0d5f829740
Oleg <init.go@gmail.com>
2023-08-29 23:32:16 +0300

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int[] GetRandomArray()
+{
+    int[] array = new int[8] ;
+    for(int i = 0; i< array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+}
+void PrintArray(int[] arrayToPrint)
+{
+    System.Console.Write("[ ");
+    for(int i = 0; i< arrayToPrint.Length; i++)
+    {
+        Console.Write($"{arrayToPrint[i]} ");
+    }
+    System.Console.WriteLine("]");
+}
+int[] userArray = GetRandomArray();
+PrintArray(userArray);
