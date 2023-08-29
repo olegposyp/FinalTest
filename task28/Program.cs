58c1dd027874fcee73fc88681ec688e3ccd54c04
Oleg <init.go@gmail.com>
2023-08-29 17:39:47 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,12 @@
+﻿int ProdOfNumber(int number)
+{
+    int prod = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        prod *= i;
+    }
+    return prod;
+}
+System.Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел до числа {usernumber} равно {ProdOfNumber(usernumber)}");
