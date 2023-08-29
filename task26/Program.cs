bd6817875de7783d12ba2fcc372555d3a3550847
Oleg <init.go@gmail.com>
2023-08-29 14:57:04 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,13 @@
+﻿int QuanOfFigures(int number)
+{
+    int sum=0;
+    while (number>0)
+    {
+    sum ++;
+    number = number/10;
+}
+return sum;
+}
+System.Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Колоичество цифр в числе {usernumber} равна {QuanOfFigures(usernumber)}");
