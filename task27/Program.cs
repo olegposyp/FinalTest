347159c6c0a1aa6a1ffa009c466a01c51bcc7614
Oleg <init.go@gmail.com>
2023-08-30 00:27:29 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int SumOfFigures(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+    return sum;
+}
+System.Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {usernumber} равно {SumOfFigures(usernumber)}");
