7c9da6223108591e9cdd61c12615cec9218d6d18
Oleg <init.go@gmail.com>
2023-08-29 14:08:24 +0300

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumOfNumber(int number)
+{
+    int sum=0;
+for (int i=1; i<=number; i++)
+{
+    sum += i;
+}
+return sum;
+}
+System.Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {usernumber} равна {SumOfNumber(usernumber)}");
