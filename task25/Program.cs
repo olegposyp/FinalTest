3e4444998fb5972d07a5f3c3e3233790ce2933eb
Oleg <init.go@gmail.com>
2023-08-29 23:52:09 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int DegreeOfNumber(int number, int degree)
+{
+    int numdeg = number;
+    for (int i = 1; i < degree; i++)
+    {
+        numdeg *= number;
+    }
+    return numdeg;
+}
+System.Console.WriteLine("Введите два числа");
+int usernumber1 = Convert.ToInt32(Console.ReadLine());
+int usernumber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {usernumber1} в степени {usernumber2} равно {DegreeOfNumber(usernumber1,usernumber2)}");
