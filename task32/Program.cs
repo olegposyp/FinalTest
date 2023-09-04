f7819713fa3e8d0cb355b9e98a5d3a3bc342e077
Oleg <init.go@gmail.com>
2023-09-04 23:35:32 +0300

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+int[] GetRandomArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+void PrintArray(int[] arrayToPrint)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write($"{arrayToPrint[i]} ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] ChangeSignArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+     array[i]*=-1;  
+    }
+    return array;
+}
+Console.WriteLine("Введите размер массива");
+int userlength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числовые границы");
+int userstart = Convert.ToInt32(Console.ReadLine());
+int userend = Convert.ToInt32(Console.ReadLine());
+int[] userArray = GetRandomArray(userlength,userstart,userend);
+PrintArray(userArray);
+PrintArray(ChangeSignArray(userArray));
