d9fb966a2ecf784fe871cdcdd4180a082817cc54
Oleg <init.go@gmail.com>
2023-09-05 22:59:53 +0300

    task42

---
+++ task42/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+string ConvBin(int num)
+{
+    string result = String.Empty;
+    int temp = 0;
+    for (int i = num; i > 0; i/=2)
+    {
+        temp = i%2;
+        result = temp + result;
+    }
+    return result;
+}
+int usnum = Convert.ToInt32(Console.ReadLine());
+
+string converted = ConvBin(usnum);
+Console.WriteLine($"Число {usnum} в двоичной форме {converted}");
