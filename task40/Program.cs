48d97b40f0d75d3646d627a67fb5adb3ac8c5257
Oleg <init.go@gmail.com>
2023-09-05 22:37:42 +0300

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+int[] FillUserArray()
+{
+    int[] array = new int[3];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+bool TriExist(int[] array)
+{
+        if (array[0]+array[1]>array[2] && array[0]+array[2]>array[1] && array[0]+array[2]>array[1])
+    {
+        return true;
+    }
+    else 
+    return false;
+}
+System.Console.WriteLine("Введите длины сторон треугольника");
+int[] userArray = FillUserArray();
+bool result = TriExist(userArray);
+System.Console.WriteLine();
+if (result==true)
+{
+    System.Console.WriteLine("Треугольник c такими длинами сторон существует");
+}
+else
+System.Console.WriteLine("Tреугольник c такими длинами сторон не существует");
