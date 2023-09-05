efc4968d5895cafbf16215fef0dbc27f7454e452
Oleg <init.go@gmail.com>
2023-09-05 12:40:39 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+int[] GetRandomArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+    return array;
+}
+void PrintArray(int[] arrayToPrint)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write($"{arrayToPrint[i]} ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int GetSumOdd(int[] array)
+{
+    int sumOdd = 0;
+    for(int i = 0; i< array.Length; i++)
+    {
+        if (array[i] %2 > 0 || array[i] %2 < 0)
+        sumOdd += array[i];
+    }
+    return sumOdd;
+}
+System.Console.WriteLine("Введите размер массива");
+int arlength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числовые границы");
+int start = Convert.ToInt32(Console.ReadLine());
+int end = Convert.ToInt32(Console.ReadLine());
+
+int[] userarray = GetRandomArray(arlength, start, end);
+PrintArray(userarray);
+System.Console.WriteLine();
+int usersumOdd = GetSumOdd(userarray);
+System.Console.WriteLine($"Сумма  чисел на нечётных позициях в массиве равна {usersumOdd}");
