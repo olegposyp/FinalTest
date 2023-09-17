e9fb796d2e1d35e429ed3f34c0d9845dabe6722e
Oleg <init.go@gmail.com>
2023-09-17 23:02:58 +0300

    task65

---
+++ task65/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+string numb = string.Empty;
+
+void NatSpace(int m, int n)
+{
+    if (m < n) 
+    {
+        System.Console.Write(m+" ");
+    NatSpace(m+1, n);
+    }
+}
+
+System.Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+NatSpace(m,n);
+System.Console.WriteLine();
