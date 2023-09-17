772dcf01212bf1b35a39f39e3933f633579eee03
Oleg <init.go@gmail.com>
2023-09-17 23:38:08 +0300

    task69

---
+++ task69/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int Degree(int m,int n)
+{
+    if (n == 1)
+    {
+        return m;
+    }
+    else
+    return m*Degree(m,n-1);
+}
+Console.Clear();
+System.Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int deg = Degree(m,n);
+Console.WriteLine($"Число {m} в степени {n} равно {deg}");
