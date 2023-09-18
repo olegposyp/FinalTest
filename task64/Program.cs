e7b8a02cd13c697e744cf396bd71196c49d14f56
Oleg <init.go@gmail.com>
2023-09-18 09:58:26 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+string numb = string.Empty;
+
+void NatNum(int end)
+{   
+    if (end>0) 
+    {
+    numb = numb+" "+Convert.ToString(end);
+    NatNum(end-1);
+    }
+}
+
+System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+NatNum(n);
+System.Console.WriteLine(numb);
