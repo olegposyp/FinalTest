a1cbdcd99d63ffb1b15a256fd7fa80f1b025ba6d
Oleg <init.go@gmail.com>
2023-09-18 10:13:33 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int sum = 0;
+void NatSumSp(int m, int n)
+{
+    if (m <= n) 
+    {
+        sum = sum+m;
+        NatSumSp(m+1, n);
+        }
+}
+
+System.Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+NatSumSp(m,n);
+System.Console.WriteLine($"Сумма чисел от {m} до {n} равна {sum}");
